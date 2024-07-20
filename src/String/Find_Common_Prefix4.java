31957003eae1420d2ffacfe0cd6efc490fffd807
Jeevith-U <102320065+Jeevith-U@users.noreply.github.com>
2024-07-20 16:47:33 +0530

    Your commit message

--- src/String/Find_Common_Prefix4.java
+++ src/String/Find_Common_Prefix4.java
@@ -8,13 +8,13 @@ public class Find_Common_Prefix4 {
 		
 		String prefix = strs[0] ;
 		
+		
 		if(strs == null || strs.length == 0) System.out.println("The Array is Empty");
 		
 		for(int i = 1 ; i < strs.length ; i++) {
 			
 			while(strs[i].indexOf(prefix) != 0) {
 				prefix = prefix.substring(0, prefix.length()-1) ;
-				System.out.println(prefix);
 			}
 		}
 		
