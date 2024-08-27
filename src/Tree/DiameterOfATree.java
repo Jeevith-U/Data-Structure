5f10904fd52c47576bcb5e6edff7d20fc709b8aa
Jeevith-U <102320065+Jeevith-U@users.noreply.github.com>
2024-08-27 11:17:10 +0530

    New Problem has been added to calculate the diameter of a given tree.

---
+++ src/Tree/DiameterOfATree.java
@@ -0,0 +1,50 @@
+package Tree;
+
+/*
+    - there are two possible ways of this problem they are
+    - i) Diameter passing through the root
+    - ii) Diameter without passing through the root
+    - there is another scenario that needed to be considered in case ( ii )
+    - that the max diameter may present in left sub tree or right sub tree or including root ie. leftmaxsubtree + rightmaxsubtree + 1
+    - the above approach will take O(n2)
+
+ */
+public class DiameterOfATree {
+
+    public static void main(String[] args) {
+
+        int arr [] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+
+        BuildATreeUsingPreOrder.Node headNode = BuildATreeUsingPreOrder.BinaryTree.buildTree(arr);
+
+        int height = calculateDiameter(headNode) ;
+
+        System.out.println("The total Height is : "+height);
+    }
+
+    private static int findHeight(BuildATreeUsingPreOrder.Node headNode) {
+
+        if(headNode == null ) return 0 ;
+
+        int leftHeight = findHeight(headNode.left) ;
+
+        int rightHeight = findHeight(headNode.right) ;
+
+        int curHeight = Math.max(leftHeight, rightHeight) + 1 ;
+
+        return curHeight ;
+    }
+
+    private static int calculateDiameter(BuildATreeUsingPreOrder.Node headNode) {
+
+        if(headNode == null) return 0 ;
+
+        int leftDia = calculateDiameter(headNode.left) ;
+
+        int rightDia = calculateDiameter(headNode.right) ;
+
+        int totDia = findHeight(headNode.left) + findHeight(headNode.right) + 1 ;
+
+        return  Math.max(totDia, Math.max(leftDia, rightDia)) ;
+    }
+}
